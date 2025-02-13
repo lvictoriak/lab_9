c3b4b5be41494fd74ff861e4b034c110795ccbc0
lvictoriak <vika7667@list.ru>
2025-02-13 13:50:37 +0500

    Third Part + Demonstrating Progran for 1 & 2
    Почти сделана третья часть, а именно коллекция. Добавлены демонстрационные программы для 1 и 2 части

--- CarArray.cs
+++ CarArray.cs
@@ -1,13 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Лаб_раб_9
 {
-    internal class CarArray
+    public class CarArray
     {
+        static Random random = new Random();
+        Car[]cars; //одномерный массив элементов типа Car
+        static int count = 0;
 
+        public int Length
+        {
+            get => cars.Length;
+        }
+
+        //Конструктор без параметров
+        public CarArray()
+        {
+            cars = new Car[0];
+            count++;
+        }
+        //Конструктор с параметрами, заполняющий элементы случайными значениями
+        public CarArray(int length)
+        {
+            cars = new Car[length];
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                cars[i] = new Car(random.Next(5, 20), random.Next(10, 70));
+            }
+            count++;
+        }
+        //Конструктор копирования, позволяющий создать копию коллекции, которая передается в конструктор как параметр, д.б. реализовано глубокое копирование
+        public CarArray(CarArray other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException("other");
+            }
+            cars =  new Car[other.cars.Length];
+            for (int i = 0; i < other.cars.Length; i++)
+            {
+                cars[i] = new Car(other.cars[i]);
+            }
+
+            count++;
+        }
+        //Метод для просмотра элементов массива
+        public void PrintInfoCars()
+        {
+            foreach (Car car in cars)
+            {
+                Console.WriteLine(car.ToString());
+            }
+        }
+        //Посчет кол-ва созданных объектов и коллекций
+        public static int GetCount => count;
     }
 }
--- Program.cs
+++ Program.cs
@@ -20,8 +20,7 @@ namespace Лаб_раб_9
             {
                 Console.WriteLine(ArgumentException.Message);
                 return null;
-            }
-            
+            }  
         }
 
         // Вывод информации об объекте
@@ -30,17 +29,163 @@ namespace Лаб_раб_9
             Console.WriteLine($"Расход топлива: {car.FuelFlow} л/100км");
             Console.WriteLine($"Объем топлива: {car.FuelVolume} л");
             Console.WriteLine($"Запас хода: {car.CalculateRange()} км");
+            Console.WriteLine();
         }
         static void Main(string[] args)
         {
-            Console.WriteLine(); 
+            int answer = 0; //ответ для демонстрационного меню
+            bool isReadAnswer = false; //для проверки ввода ответа
+
+          
+            do
+            {
+                Console.WriteLine("1. First part. Demonstrate");
+                Console.WriteLine("2. Second part. Demonstarte");
+                Console.WriteLine("3. Third part. Demonstrate");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine();
+                do
+                {
+                    isReadAnswer = int.TryParse(Console.ReadLine(), out answer);
+                    if (answer < 1) isReadAnswer = false;
+                    if (answer > 4) isReadAnswer = false;
+                    if (!isReadAnswer)
+                        Console.WriteLine("Ошибка при вводе пункта меню, попробуйте снова. Выбирайте только из предложенных, от 1 до 4.");
+                } while (!isReadAnswer);
+
+                switch (answer)
+                {
+                    case 1:
+                        Console.WriteLine("Создание элемента типа конструктором без параметров");
+                        Car car1 = new Car();
+                        PrintInfo(car1);
+
+                        Console.WriteLine("Создание элемента типа конструктором с параметрами");
+                        Car car2 = CreateElem(10, 10);
+                        PrintInfo(car2);
+
+                        Console.WriteLine("Проверка создания элемента с некорректными данными");
+                        Car car3 = CreateElem(-10, 10);
+                        Console.WriteLine();
+                        
+                        Console.WriteLine("Использование конструктора копирования");
+                        Car car4 = new Car(car2);
+                        PrintInfo(car4);
+
+                        Console.WriteLine("Вычисления запаса хода");
+                        Car carToCalcRange = CreateElem(14, 50);
+                        Console.WriteLine($"Запас хода = { carToCalcRange.CalculateRange()}");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Вычиление запаса хода статической функцией");
+                        Car carToCalcRangeSt = CreateElem(14, 50);
+                        Console.WriteLine($"Запас хода = {Car.CalculateRangeStatic(carToCalcRangeSt)}");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Вывод кол-ва созданных элементов");
+                        Console.WriteLine($"Кол-во созданных элементов = {Car.GetCount}");
+                        break;
+                    case 2:
+                        Console.WriteLine("Унарные операции");
+                        Console.WriteLine("Демонстрация операции ++(увеличение расхода топлива на 0,1 л/100 км)");
+                        Car car10 = CreateElem(10, 10);
+                        car10 = car10++;
+                        PrintInfo(car10);
+
+                        Console.WriteLine("Демонстрация операции --(уменьшение топлива в баке на 1 л, при условии что оно не уйдет в минус");
+                        Car car11 = CreateElem(10, 10);
+                        car11 = car11--;
+                        PrintInfo(car11);
+                        Console.WriteLine();
+                        
+                        Console.WriteLine("Операции типа приведения");
+                        Console.WriteLine("Операции bool, правда, true, если автомобиль сможет доехать до заправки (до заправки ровно 100 км), а в баке в момент заправки останется не меньше 5 л топлива, иначе – false");
+                        Car car12 = new Car(10, 30);
+                        Console.WriteLine("Элемент, над которым выполняет операцию");
+                        PrintInfo(car12);
+                        if ((bool)car12) Console.WriteLine("Сможет доехать до заправки");
+                        else
+                            Console.WriteLine("Не сможет доехать до заправки");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Операция double, результат - количество сотен километров до заправки, чтобы в момент заправки в баке осталось ровно 5 л топлива. Если в момент расчёта в баке меньше 5 л топлива, значит результатом операции будет число\r\n-1\r\n");
+                        Console.WriteLine("Элемент, над которым выполняет операцию");
+                        PrintInfo(car12);
+                        if ((double)car12 == -1) Console.WriteLine("Доехать до заправки авто не сможет");
+                        else
+                            Console.WriteLine($"До заправки сможет проехать {(double)car12} сотен километров, чтобы в момент заправки осталось 5 литров в баке");
+                        Console.WriteLine();
+                        
+                        Console.WriteLine("Бинарные операции");
+                        Console.WriteLine("+, левосторонняя операция добавления некоторого кол-ва литров топлива");
+                        Console.WriteLine("Элемент, над которым выполняет операцию");
+                        PrintInfo(car12);
+                        Console.WriteLine("Выполняем операцию, добавим 10 л");
+                        car12 = car12 + 10;
+                        PrintInfo(car12);
+                        Console.WriteLine();
+
+                        Console.WriteLine("+, правосторонняя операция уменьшения расхода топлива на заданное число");
+                        Console.WriteLine("Элемент, над которым выполняет операцию");
+                        PrintInfo(car12);
+                        Console.WriteLine("Выполняем операцию, уменьшим расход топлива на 2 л/100 км");
+                        car12 = 2 + car12;
+                        PrintInfo(car12);
+                        Console.WriteLine();
+
+                        Console.WriteLine("==, автомобили имеют равные возможности, если равны их атрибуты");
+                        Console.WriteLine("Первый авто для сравнений");
+                        PrintInfo(car11);
+                        Console.WriteLine("Второй авто для сравнений");
+                        PrintInfo(car12);
+                        if (car11 == car12) Console.WriteLine("Автомобилии равносильны");
+                        else
+                            Console.WriteLine("Автомобили не равносильны");
+                        Console.WriteLine();
+
+                        Car car12Copy = new Car(car12);
+                        Console.WriteLine("Первый авто для сравнений");
+                        PrintInfo(car12Copy);
+                        Console.WriteLine("Второй авто для сравнений");
+                        PrintInfo(car12);
+                        if (car12Copy == car12) Console.WriteLine("Автомобилии равносильны");
+                        else
+                            Console.WriteLine("Автомобили не равносильны");
+                        Console.WriteLine();
+
+                        Console.WriteLine("!=, автомобили не равносильны, если не равны их атрибуты");
+                        Console.WriteLine("Первый авто для сравнений");
+                        PrintInfo(car11);
+                        Console.WriteLine("Второй авто для сравнений");
+                        PrintInfo(car12);
+                        if (car11 != car12) Console.WriteLine("Автомобилии не равносильны");
+                        else
+                            Console.WriteLine("Автомобили равносильны");
+                        Console.WriteLine();
+
+                        Console.WriteLine("Первый авто для сравнений");
+                        PrintInfo(car12Copy);
+                        Console.WriteLine("Второй авто для сравнений");
+                        PrintInfo(car12);
+                        if (car12Copy != car12) Console.WriteLine("Автомобилии не равносильны");
+                        else
+                            Console.WriteLine("Автомобили равносильны");
+                        Console.WriteLine();
+
+                        break;
+                    case 3:
+
+                        break;
+                }
+            } while (answer < 4);
             
             
             
             Car c1 = CreateElem(10, 25);
             PrintInfo(c1);
             Console.WriteLine(Car.GetCount);
-            
+            Car car5 = CreateElem(-10, 10);
+
         }
     }
 }
